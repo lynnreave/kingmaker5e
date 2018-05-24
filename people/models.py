859ec0a4b059f80274590166a036c38fd76d7761
Sephone Slattery <sephone@multiscalehn.com>
2018-05-24 13:35:09 -0700

    Attempting fix for deployment with requirements file.

--- people/models.py
+++ people/models.py
@@ -2,8 +2,6 @@ from django.db import models
 
 default_max_length = 100
 
-# Create your models here.
-
 
 class Gender(models.Model):
     name = models.CharField(max_length=default_max_length)
@@ -28,7 +26,6 @@ class Person(models.Model):
     last_name = models.CharField(max_length=default_max_length)
     gender = models.OneToOneField(Gender, on_delete=models.CASCADE)
     noble_rank = models.OneToOneField(NobleRank, on_delete=models.CASCADE, blank=True, null=True)
-    #awards = models.ManyToManyField(Award, blank=True, related_name='persons', related_query_name='person')
     str = models.IntegerField(default=10)
     dex = models.IntegerField(default=10)
     con = models.IntegerField(default=10)
