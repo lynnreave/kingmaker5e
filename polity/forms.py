050a18f90e1f5175891cf992b8140725216b97c9
Sephone Slattery <sephone@multiscalehn.com>
2018-05-27 19:27:14 -0700

    Minor tweaks to view.

--- polity/forms.py
+++ polity/forms.py
@@ -7,7 +7,9 @@ class PolityForm(forms.ModelForm):
     class Meta:
         model = Polity
         fields = (
-            'name', 'government', 'alignment_lc', 'alignment_ge', 'desc',
+            'name', 'government', 'alignment_lc', 'alignment_ge',
+            'treasury', 'unrest',
+            'desc',
             'tax_edict', 'promotion_edict', 'holiday_edict', 'recruitment_edict',
             'ruler_attribute_1', 'ruler_attribute_2', 'ruler_attribute_3', 'spymaster_attribute'
         )
